a52103cf814697a360367e5065cc1c1374e70530
Yukine <blazylp@gmail.com>
2019-05-09 13:25:59 +0200

    [Fix] ClientStatus properties are nullable

--- Spectacles.NET.Types/Presence.cs
+++ Spectacles.NET.Types/Presence.cs
@@ -38,19 +38,19 @@ namespace Spectacles.NET.Types
 		/// the user's status set for an active desktop (Windows, Linux, Mac) application session
 		/// </summary>
 		[JsonProperty("desktop")]
-		public Status Desktop { get; set; }
+		public Status? Desktop { get; set; }
 		
 		/// <summary>
 		/// the user's status set for an active mobile (iOS, Android) application session
 		/// </summary>
 		[JsonProperty("mobile")]
-		public Status Mobile { get; set; }
+		public Status? Mobile { get; set; }
 		
 		/// <summary>
 		/// the user's status set for an active web (browser, bot account) application session
 		/// </summary>
 		[JsonProperty("web")]
-		public Status Web { get; set; }
+		public Status? Web { get; set; }
 	}
 	
 	/// <summary>
