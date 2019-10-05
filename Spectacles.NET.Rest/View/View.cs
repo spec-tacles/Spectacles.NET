08233533c9e64e3ea3d50291a11f39adf4577800
Yukine <blazylp@gmail.com>
2019-10-05 21:35:55 +0200

    [Fix] Fix IFile import path

--- Spectacles.NET.Rest/View/View.cs
+++ Spectacles.NET.Rest/View/View.cs
@@ -4,7 +4,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Spectacles.NET.Rest.Util;
+using Spectacles.NET.Types;
 
 namespace Spectacles.NET.Rest.View
 {
