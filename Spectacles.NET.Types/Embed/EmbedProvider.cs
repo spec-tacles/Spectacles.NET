39391b7e39890b3587426d9a256e878d84799022
Veld <veldsinkie.mv@gmail.com>
2020-05-01 23:03:16 +0200

    Fix indentation

--- Spectacles.NET.Types/Embed/EmbedProvider.cs
+++ Spectacles.NET.Types/Embed/EmbedProvider.cs
@@ -2,22 +2,22 @@ using System.Runtime.Serialization;
 
 namespace Spectacles.NET.Types
 {
-    /// <summary>
-    /// Provider of an Embed
-    /// </summary>
-    [DataContract]
-    public class EmbedProvider
-    {
-        /// <summary>
-        ///     name of provider
-        /// </summary>
-        [DataMember(Name = "name", Order = 1)]
-        public string Name { get; set; }
+	/// <summary>
+	/// Provider of an Embed
+	/// </summary>
+	[DataContract]
+	public class EmbedProvider
+	{
+		/// <summary>
+		///     name of provider
+		/// </summary>
+		[DataMember(Name = "name", Order = 1)]
+		public string Name { get; set; }
 
-        /// <summary>
-        ///     url of provider
-        /// </summary>
-        [DataMember(Name = "url", Order = 2)]
-        public string URL { get; set; }
-    }
+		/// <summary>
+		///     url of provider
+		/// </summary>
+		[DataMember(Name = "url", Order = 2)]
+		public string URL { get; set; }
+	}
 }
--- Spectacles.NET.Types/Embed/EmbedVideo.cs
+++ Spectacles.NET.Types/Embed/EmbedVideo.cs
@@ -2,28 +2,28 @@ using System.Runtime.Serialization;
 
 namespace Spectacles.NET.Types
 {
-    /// <summary>
-    /// Video of an Embed
-    /// </summary>
-    [DataContract]
-    public class EmbedVideo
-    {
-        /// <summary>
-        ///     source url of video
-        /// </summary>
-        [DataMember(Name = "url", Order = 1)]
-        public string URL { get; set; }
+	/// <summary>
+	/// Video of an Embed
+	/// </summary>
+	[DataContract]
+	public class EmbedVideo
+	{
+		/// <summary>
+		///     source url of video
+		/// </summary>
+		[DataMember(Name = "url", Order = 1)]
+		public string URL { get; set; }
 
-        /// <summary>
-        ///     height of video
-        /// </summary>
-        [DataMember(Name = "height", Order = 2)]
-        public int? Height { get; set; }
+		/// <summary>
+		///     height of video
+		/// </summary>
+		[DataMember(Name = "height", Order = 2)]
+		public int? Height { get; set; }
 
-        /// <summary>
-        ///     width of video
-        /// </summary>
-        [DataMember(Name = "width", Order = 3)]
-        public int? Width { get; set; }
-    }
+		/// <summary>
+		///     width of video
+		/// </summary>
+		[DataMember(Name = "width", Order = 3)]
+		public int? Width { get; set; }
+	}
 }
