3d33c50e6439ea4c6850bd035b8b74e24182df97
Veld <veldsinkie.mv@gmail.com>
2020-05-01 22:57:23 +0200

    Update AuditLogChange.cs

--- Spectacles.NET.Types/AuditLogs/AuditLogChange/AuditLogChange.cs
+++ Spectacles.NET.Types/AuditLogs/AuditLogChange/AuditLogChange.cs
@@ -4,12 +4,14 @@ namespace Spectacles.NET.Types
 	/// Audit Log Change with the any type
 	/// </summary>
 	public class AuditLogChange<T> : AuditLogChangeBase, IAuditLogChange<T>
-    {
-        /// <inheritdoc />
-        public new T NewValue => (T) base.NewValue;
+	{
+		/// <inheritdoc />
+		public new T NewValue
+			=> (T) base.NewValue;
 
 
-        /// <inheritdoc />
-        public new T OldValue => (T) base.OldValue;
-    }
+		/// <inheritdoc />
+		public new T OldValue
+			=> (T) base.OldValue;
+	}
 }
